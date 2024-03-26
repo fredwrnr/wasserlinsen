103729600f418577981347ece3a2b750bf380502
fred <fred.w@posteo.de>
2024-03-26 16:33:28 +0100

    updated

--- svm/gui.py
+++ svm/gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
 from tkinter import messagebox
-import model_trainer
+import lemna_master
 import os
 import glob
 from PIL import Image, ImageTk
@@ -19,13 +19,12 @@ class AppGUI:
         self.root = root
         root.title("Lemna-Master5000")
 
+        self.lemna_master = lemna_master.LemnaMaster()
+
         # Search for .sav files in the current directory and use the first one found
         self.current_directory = os.getcwd()
         self.model_path = get_first_model_path(self.current_directory, ".sav")
-        if self.model_path:
-            self.model = model_trainer.load_model(self.model_path)
-        else:
-            self.model = None
+        self.lemna_master.load_model(self.model_path)
 
         # Button to select a different model
         self.change_model_btn = tk.Button(root, text="Select Model", command=self.change_model)
@@ -35,12 +34,9 @@ class AppGUI:
         self.model_label = tk.Label(root, text=model_display_text)
         self.model_label.pack(pady=5)
 
-
         # Training button and label
         self.train_btn = tk.Button(root, text="Train new Model", command=self.train_model)
         self.train_btn.pack(pady=10)
-        #self.train_folder_label = tk.Label(root, text="No folder selected for training")
-        #self.train_folder_label.pack(pady=5)
 
         # Initialize the BooleanVar to hold the toggle state
         self.toggle_state = tk.BooleanVar(value=False)
@@ -57,6 +53,10 @@ class AppGUI:
         self.progress = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=200, mode='determinate')
         self.progress.pack(pady=10)
 
+        self.inference_cancelled = False
+        self.cancel_button = tk.Button(self.root, text="Cancel Inference", command=self.request_stop_inference)
+        self.cancel_button.pack()  # Adjust the layout according to your GUI design
+
         self.plant_area_label = tk.Label(root, text="")
         self.plant_area_label.pack(pady=5, padx=20)
         # Image display frames
@@ -65,6 +65,9 @@ class AppGUI:
 
         self.image_label = tk.Label(self.frame)
         self.image_label.pack()
+
+    def request_stop_inference(self):
+        self.inference_cancelled = True
 
     def display_image(self, image):
         """Display images in the application."""
@@ -76,24 +79,21 @@ class AppGUI:
 
     def change_model(self):
         """Open a dialog for the user to select a new model file."""
-        new_model_path = filedialog.askopenfilename(initialdir=self.current_directory, title="Select Model File",
+        new_model_path = filedialog.askopenfilename(initialdir=self.current_directory, title="Select Model File, must end with '.sav'",
                                                     filetypes=[("Model files", "*.sav")])
         if new_model_path:
             self.model_path = new_model_path
-            self.model = model_trainer.load_model(self.model_path)
+            self.lemna_master.load_model(self.model_path)
             self.model_label.config(text=f"Current Model: {os.path.basename(self.model_path)}")
             messagebox.showinfo("Model Changed", f"Model changed to {os.path.basename(self.model_path)}.")
 
-    def select_folder(self):
-        folder_path = filedialog.askdirectory()
-        return folder_path
 
     def train_model(self):
-        folder_path = self.select_folder()
+        folder_path = filedialog.askdirectory()
         if folder_path:
             #self.train_folder_label.config(text=f"Training folder: {folder_path}")
 
-            model, path, accuracy = model_trainer.train_svm(folder_path, self.toggle_state.get())
+            model, path, accuracy = self.lemna_master.train_svm(folder_path, self.toggle_state.get())
             self.model = model  # Update the current model
             self.model_label.config(text=f"Current Model: {path}")
             messagebox.showinfo("Training Complete\n", f"The model has been trained and saved successfully. \n"
@@ -101,23 +101,22 @@ class AppGUI:
         else:
             messagebox.showwarning("Training Aborted", "No folder selected for training.")
 
-    def run_inference(self):
+    """def run_inference(self):
         if not self.model:
             messagebox.showwarning("Inference Error", "Please load or train a model before running inference.")
             return
-        folder_path = self.select_folder()
+        folder_path = filedialog.askdirectory()
         if folder_path:
             self.infer_folder_label.config(text=f"Inference folder: {folder_path}")
             self.progress['value'] = 0
             self.root.update_idletasks()
 
-
-            files_to_test = model_trainer.read_all_images_in_folder(folder_path)
+            files_to_test = self.lemna_master.read_all_images_in_folder(folder_path)
             self.root.config(cursor="watch")
             self.root.update()
             for i, image_path in enumerate(files_to_test):
                 print(f"Processing {image_path}")
-                output_image, plant_percentage = model_trainer.run_inference(self.model, os.path.join(folder_path, image_path))
+                output_image, plant_percentage = self.lemna_master.run_inference_on_image(os.path.join(folder_path, image_path))
                 self.plant_area_label["text"] = f"Pflanzenfläche: {plant_percentage} %"
                 self.display_image(output_image)
                 # Update progress in GUI
@@ -126,7 +125,48 @@ class AppGUI:
             self.root.config(cursor="")
             messagebox.showinfo("Inference Complete", "Inference has been run successfully.")
         else:
-            messagebox.showwarning("Inference Aborted", "No folder selected for inference.")
+            messagebox.showwarning("Inference Aborted", "No folder selected for inference.")"""
+
+    def run_inference(self):
+        if not self.lemna_master.loaded_model:
+            self.lemna_master.load_model(self.model_path)
+        inference_folder_path = filedialog.askdirectory()
+        if inference_folder_path:
+            self.infer_folder_label.config(text=f"Inference folder: {inference_folder_path}")
+            self.progress['value'] = 0
+            self.root.update_idletasks()
+
+            self.root.config(cursor="watch")
+            threading.Thread(target=self.inference_thread, args=(inference_folder_path,)).start()
+            self.root.config(cursor="")
+
+    def inference_thread(self, folder_path):
+        self.inference_cancelled = False  # Reset at the start
+        files_to_test = self.lemna_master.read_all_images_in_folder(folder_path)
+        for i, image_path in enumerate(files_to_test):
+            if self.inference_cancelled:
+                break  # Stop processing if cancellation is requested
+            print(f"Processing {image_path}")
+            output_image, plant_percentage = self.lemna_master.run_inference_on_image(
+                os.path.join(folder_path, image_path))
+            self.update_gui_after_inference(output_image, plant_percentage, i, len(files_to_test))
+
+        if self.inference_cancelled:
+            self.root.after(0, lambda: messagebox.showinfo("Inference Cancelled", "Inference has been cancelled."))
+        else:
+            self.root.after(0,
+                            lambda: messagebox.showinfo("Inference Complete", "Inference has been run successfully."))
+
+        self.inference_cancelled = False  # Reset at the end
+
+    def update_gui_after_inference(self, output_image, plant_percentage, i, total):
+        def update():
+            self.plant_area_label["text"] = f"Pflanzenfläche: {plant_percentage} %"
+            self.display_image(output_image)
+            progress = int((i + 1) / total * 100)
+            self.update_progress(progress)
+
+        self.root.after(0, update)
 
     def update_progress(self, value):
         self.progress['value'] = value
---
+++ svm/lemna_master.py
@@ -0,0 +1,207 @@
+import numpy as np
+import csv
+import pickle
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, mean_squared_error, make_scorer
+from sklearn.model_selection import GridSearchCV, train_test_split
+import os
+import cv2
+from tqdm import tqdm
+import time
+
+
+class LemnaMaster:
+    def __init__(self):
+        self.pt1 = (1150,580)    # x,y
+        self.pt2 = (3000,2410)
+        self.loaded_model = None
+
+    def load_model(self, model_path):
+        self.loaded_model = pickle.load(open(model_path, 'rb'))
+
+    @staticmethod
+    def delete_small_blobs(image, size_threshold):
+        """
+        Deletes blobs in an image that are smaller than a certain threshold.
+        Blobs are identified as connected regions with pixel values [255, 0, 0].
+        The background is assumed to be [0, 0, 0].
+
+        Args:
+        - image (np.ndarray): The input image as a NumPy array of shape (height, width, 3).
+        - size_threshold (int): The size threshold for blob deletion.
+
+        Returns:
+        - np.ndarray: The processed image with small blobs removed.
+        """
+        # Convert image to grayscale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Create a binary mask where red blobs are white and the rest is black
+        red_blobs_mask = np.all(image == [255, 0, 0], axis=-1).astype(np.uint8) * 255
+
+        # Find connected components in the binary mask
+        num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(red_blobs_mask, 8, cv2.CV_32S)
+
+        # Create an output image that starts as a copy of the original
+        output_image = image.copy()
+
+        # Iterate through found components, excluding the background (label 0)
+        for label in range(1, num_labels):
+            # If the component size is smaller than the threshold, delete it
+            if stats[label, cv2.CC_STAT_AREA] < size_threshold:
+                # Set the pixels corresponding to this component to the background color
+                output_image[labels == label] = [0, 0, 0]
+
+        return output_image
+
+
+    def read_all_images_in_folder(self, path=None):
+        filenames = os.listdir(path)
+        image_filenames = []
+        if len(filenames) != 0:
+            for file in filenames:
+                if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                    image_filenames.append(file)
+        return image_filenames
+
+
+    def make_dataset(self, folder, with_bad = False):
+        def on_mouse(event, x, y, flags, param):
+            if param == "mark_plant":
+                color = (0, 255, 0)
+            elif param == "mark_background":
+                color = (255, 0, 0)
+            else:
+                color = (0,0,255)
+
+            if event == cv2.EVENT_LBUTTONDOWN or (event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON):
+                image[y-2:y+2,x-2:x+2] = color
+                roi = cie_image[y-2:y+2,x-2:x+2].tolist()
+
+                for row in roi:
+                    for pixel in row:
+                        if param == "mark_plant":
+                            plant_pixels.append(pixel)
+                        elif param == "mark_background":
+                            background_pixels.append(pixel)
+                        else:
+                            bad_plant_pixels.append(pixel)
+
+        plant_pixels = []
+        background_pixels = []
+        bad_plant_pixels = []
+        for i, filename in enumerate(self.read_all_images_in_folder(folder)):
+            image = cv2.imread(os.path.join(folder, filename))
+            image = image[self.pt1[1]:self.pt2[1], self.pt1[0]:self.pt2[0]]  # get only center
+            image = cv2.resize(image, dsize=(image.shape[1] // 2, image.shape[0] // 2), interpolation=cv2.INTER_CUBIC)
+
+            cie_image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+
+            cv2.namedWindow("Bitte (gesunde) Wasserlinsen markieren", cv2.WINDOW_FULLSCREEN)
+            cv2.setMouseCallback('Bitte (gesunde) Wasserlinsen markieren', on_mouse, "mark_plant")
+            while True:
+                cv2.imshow("Bitte (gesunde) Wasserlinsen markieren", image)
+                if cv2.waitKey(1) & 0xFF == 32:
+                    break
+            cv2.destroyAllWindows()
+
+            if with_bad:
+                cv2.namedWindow("Bitte tote Wasserlinsen markieren", cv2.WINDOW_FULLSCREEN)
+                cv2.setMouseCallback('Bitte tote Wasserlinsen markieren', on_mouse, "mark_bad")
+                while True:
+                    cv2.imshow("Bitte tote Wasserlinsen markieren", image)
+                    if cv2.waitKey(1) & 0xFF == 32:
+                        break
+                cv2.destroyAllWindows()
+
+            cv2.namedWindow("Bitte Hintergrund markieren", cv2.WINDOW_FULLSCREEN)
+            cv2.setMouseCallback('Bitte Hintergrund markieren', on_mouse, "mark_background")
+            while True:
+                cv2.imshow("Bitte Hintergrund markieren", image)
+                if cv2.waitKey(1) & 0xFF == 32:
+                    break
+            cv2.destroyAllWindows()
+
+        for i, pixel in enumerate(plant_pixels):
+            plant_pixels[i] = pixel + [1]
+        for i, pixel in enumerate(bad_plant_pixels):
+            bad_plant_pixels[i] = pixel + [2]
+        for i, pixel in enumerate(background_pixels):
+            background_pixels[i] = pixel + [0]
+
+        return plant_pixels + bad_plant_pixels + background_pixels
+
+
+    def train_svm(self, folder, with_bad):
+        data = self.make_dataset(folder, with_bad)
+        train, test = train_test_split(data, test_size=0.2, random_state=1)
+
+        train_x = [row[:3] for row in train]  # Features: First three columns
+        train_y = [row[3] for row in train]  # Labels: Fourth column
+        test_x = [row[:3] for row in test]  # Features: First three columns
+        test_y = [row[3] for row in test]  # Labels: Fourth column
+
+        model = SVC(C=10, gamma=0.001, kernel="rbf")
+        model.fit(train_x, train_y)
+
+        pred = model.predict(test_x)
+        rep = classification_report(test_y, pred, output_dict=True)
+        print(rep)
+        classes = 3 if with_bad else 2
+        with open(f'svc_model_{classes}classes.sav', 'wb') as f:
+            # Pickle the 'data' dictionary using the highest protocol available.
+            pickle.dump(model, f)
+        return model, f'svc_model_{classes}classes.sav', rep["accuracy"]
+
+    def run_inference_on_image(self, image_path):
+        image = cv2.imread(image_path)
+        image = image[self.pt1[1]:self.pt2[1], self.pt1[0]:self.pt2[0]]  # get only center
+        image = cv2.resize(image, dsize=(image.shape[1] // 8, image.shape[0] // 8), interpolation=cv2.INTER_CUBIC)
+        cie_image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+        width = cie_image.shape[1]
+        height = cie_image.shape[0]
+        image_mask = cie_image.copy()
+        for x in tqdm(range(width)):
+            for y in range(height):
+                pixel = [cie_image[y, x]]
+                result = self.loaded_model.predict(pixel)
+                if result[0] == 1:
+                    image_mask[y, x] = [255, 0, 0]
+                elif result[0] == 0:
+                    image_mask[y, x] = [0, 0, 0]
+                elif result[0] == 2:
+                    image_mask[y, x] = [0, 0, 255]
+
+        image_mask = self.delete_small_blobs(image_mask, 50)
+
+        plant_area = np.count_nonzero(image_mask != 0)
+        image_area = image.shape[0] * image.shape[1]
+        plant_area_percentage = round(plant_area / image_area * 100, 2)
+        print(plant_area_percentage)
+
+        blended = cv2.addWeighted(image, 0.5, image_mask, 0.5, 0)
+
+        stacked_image = np.concatenate([image, image_mask, blended], axis=1)
+        self.save_inference_result(image_path, stacked_image, plant_area_percentage)
+
+        return cv2.cvtColor(stacked_image, cv2.COLOR_BGR2RGB), plant_area_percentage
+
+    def save_inference_result(self, image_path, stacked_image, plant_area_percentage, dead_area_percentage=None):
+        os.makedirs("output", exist_ok=True)
+        image_name = image_path.split('.')[-2]
+        cv2.imwrite(os.path.join("output", f"{image_name}_output.jpg"), stacked_image)
+        if not os.path.isfile("output.csv"):
+            with open(os.path.join("output", 'output.csv'), mode='w', newline='') as f:
+                writer = csv.writer(f, delimiter=',')
+                writer.writerow(['Filename', 'Lemna Area [%]'])
+        else:
+            with open(os.path.join("output", 'output.csv'), mode='a', newline='') as f:
+                writer = csv.writer(f, delimiter=',')
+                writer.writerow([image_name, plant_area_percentage, dead_area_percentage])
+
+
+if __name__ == "__main__":
+    pass
+    #make_dataset()
+    #train_svm()
+    #main()
